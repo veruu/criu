c12fc7904f803ceffe6fb03692d33b094c777246
Mike Rapoport <rppt@linux.vnet.ibm.com>
2017-02-06 13:44:05 +0200

    cr-check: lazy-pages: detect availability of UNMAP and EXIT events
    travis-ci: success for lazy-pages: add non-#PF events handling (rev2)
    Signed-off-by: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/cr-check.c
+++ criu/cr-check.c
@@ -1071,7 +1071,9 @@ static int check_compat_cr(void)
 static int check_uffd(void)
 {
 	unsigned long features = UFFD_FEATURE_EVENT_FORK |
+		UFFD_FEATURE_EVENT_EXIT |
 		UFFD_FEATURE_EVENT_REMAP |
+		UFFD_FEATURE_EVENT_UNMAP |
 		UFFD_FEATURE_EVENT_REMOVE;
 
 	if (kerndat_uffd(true))
