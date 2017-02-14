a4ef66b0361c30da702200220a687759d749df12
Andrei Vagin <avagin@virtuozzo.com>
2017-02-15 02:59:30 +0300

    kerndat: check the SIOCGSKNS ioctl
    This ioctl is called for a socket and returns a file descriptor
    for network namespace where a socket has been created.
    
    Acked-by: Pavel Emelyanov <xemul@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/cr-check.c
+++ criu/cr-check.c
@@ -1087,6 +1087,17 @@ static int check_uffd(void)
 	return 0;
 }
 
+static int check_sk_netns(void)
+{
+	if (kerndat_socket_netns() < 0)
+		return -1;
+
+	if (!kdat.sk_ns)
+		return -1;
+
+	return 0;
+}
+
 static int (*chk_feature)(void);
 
 /*
@@ -1190,6 +1201,7 @@ int cr_check(void)
 		ret |= check_tcp_halt_closed();
 		ret |= check_userns();
 		ret |= check_loginuid();
+		ret |= check_sk_netns();
 	}
 
 	/*
@@ -1241,6 +1253,7 @@ static struct feature_list feature_list[] = {
 	{ "tcp_half_closed", check_tcp_halt_closed },
 	{ "compat_cr", check_compat_cr },
 	{ "lazy_pages", check_uffd },
+	{ "sk_ns", check_sk_netns },
 	{ NULL, NULL },
 };
 
