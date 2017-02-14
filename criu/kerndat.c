4ec9918ecdeee69e6d4ec6afd2f19161a2a99797
Andrei Vagin <avagin@virtuozzo.com>
2017-02-15 02:59:30 +0300

    kerndat: check the SIOCGSKNS ioctl
    This ioctl is called for a socket and returns a file descriptor
    for network namespace where a socket has been created.
    
    Acked-by: Pavel Emelyanov <xemul@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/kerndat.c
+++ criu/kerndat.c
@@ -27,6 +27,7 @@
 #include "proc_parse.h"
 #include "config.h"
 #include "sk-inet.h"
+#include "sockets.h"
 #include <compel/plugins/std/syscall-codes.h>
 #include <compel/compel.h>
 #include "netfilter.h"
@@ -717,6 +718,8 @@ int kerndat_init(void)
 		ret = kerndat_has_memfd_create();
 	if (!ret)
 		ret = kerndat_uffd(opts.lazy_pages);
+	if (!ret)
+		ret = kerndat_socket_netns();
 
 	kerndat_lsm();
 	kerndat_mmap_min_addr();
