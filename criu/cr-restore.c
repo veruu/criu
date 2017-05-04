97345d3f02d9c16f1b28c34a3195b623668f57e5
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-05-04 21:42:36 +0300

    images: Implement collect_images() to minimaze code lines
    Pack repeated actions in one. Also, fixed the "return -1"
    from the middle of restore_task_with_children(),
    while cleanup is need.
    
    v2: goto err in case of error instead of return -1.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -327,7 +327,7 @@ static int run_post_prepare(void)
 
 static int root_prepare_shared(void)
 {
-	int ret = 0, i;
+	int ret = 0;
 	struct pstree_item *pi;
 
 	pr_info("Preparing info about shared resources\n");
@@ -341,11 +341,8 @@ static int root_prepare_shared(void)
 	if (prepare_seccomp_filters())
 		return -1;
 
-	for (i = 0; i < ARRAY_SIZE(cinfos); i++) {
-		ret = collect_image(cinfos[i]);
-		if (ret)
-			return -1;
-	}
+	if (collect_images(cinfos, ARRAY_SIZE(cinfos)))
+		return -1;
 
 	for_each_pstree_item(pi) {
 		if (pi->pid->state == TASK_HELPER)
@@ -1734,8 +1731,6 @@ static int restore_task_with_children(void *_arg)
 
 	/* Restore root task */
 	if (current->parent == NULL) {
-		int i;
-
 		if (prepare_shared_tty())
 			goto err;
 
@@ -1755,12 +1750,8 @@ static int restore_task_with_children(void *_arg)
 		if (mount_proc())
 			goto err;
 
-		for (i = 0; i < ARRAY_SIZE(before_ns_cinfos); i++) {
-			ret = collect_image(before_ns_cinfos[i]);
-			if (ret)
-				return -1;
-		}
-
+		if (collect_images(before_ns_cinfos, ARRAY_SIZE(before_ns_cinfos)))
+			goto err;
 
 		if (prepare_namespace(current, ca->clone_flags))
 			goto err;
