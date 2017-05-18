10df36114216dfa45b5f2cb358c27b722984df2d
Pavel Emelyanov <xemul@virtuozzo.com>
2017-05-18 12:37:01 +0300

    tty: Kill prepare_shared_tty
    The routine in question just sets up the mutex to access
    /dev/ptmx. This initialization can be done when we collect
    a single tty.
    
    ✓ travis-ci: success for Sanitize initialization bits
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -1701,9 +1701,6 @@ static int restore_task_with_children(void *_arg)
 
 	/* Restore root task */
 	if (current->parent == NULL) {
-		if (prepare_shared_tty())
-			goto err;
-
 		if (join_namespaces()) {
 			pr_perror("Join namespaces failed");
 			goto err;
