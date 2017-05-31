d2f92d7caf105a440e5cca11567fcba97f1fe2ce
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-05-31 20:55:03 +0300

    ns: Restore pid_for_children ns in threads
    Threads may have different pid_for_children ns.
    Allow them to set it after they are created:
    just get a pid_ns fd from fdstore, and setns()
    to it, after thread creation.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/pie/restorer.c
+++ criu/pie/restorer.c
@@ -500,7 +500,7 @@ long __export_restore_thread(struct thread_restore_args *args)
 	k_rtsigset_t to_block;
 	unsigned long new_sp;
 	int my_pid = sys_gettid();
-	int i, ret;
+	int i, fd, ret;
 
 	for (i = 0; i < MAX_NS_NESTING; i++)
 		if (args->pid[i] == 0)
@@ -523,6 +523,16 @@ long __export_restore_thread(struct thread_restore_args *args)
 
 	if (restore_thread_common(args))
 		goto core_restore_end;
+
+	fd = args->pfc_ns_fd;
+	if (fd >= 0) {
+		ret = sys_setns(fd, CLONE_NEWPID);
+		if (ret) {
+			pr_err("Can't setns: ret=%d\n", ret);
+			goto core_restore_end;
+		}
+		sys_close(fd);
+	}
 
 	ret = restore_creds(args->creds_args, args->ta->proc_fd);
 	if (ret)
@@ -1159,7 +1169,7 @@ static int wait_zombies(struct task_restore_args *task_args)
 long __export_restore_task(struct task_restore_args *args)
 {
 	long ret = -1;
-	int i, k;
+	int i, k, fd, self_thread;
 	VmaEntry *vma_entry;
 	unsigned long va;
 	struct restore_vma_io *rio;
@@ -1484,15 +1494,16 @@ long __export_restore_task(struct task_restore_args *args)
 	 * | thread restore proc | thread1 stack | thread1 rt_sigframe |
 	 * +--------------------------------------------------------------------------+
 	 */
-
+	self_thread = 0;
 	if (args->nr_threads > 1) {
 		struct thread_restore_args *thread_args = args->thread_args;
 		long clone_flags = CLONE_VM | CLONE_FILES | CLONE_SIGHAND	|
 				   CLONE_THREAD | CLONE_SYSVSEM | CLONE_FS;
 		long last_pid_len;
 		long parent_tid;
-		int i, fd = -1;
+		int i;
 
+		fd = -1;
 		if (thread_args[0].pid[1] == 0) {
 			/* One level pid ns hierarhy */
 			fd = sys_openat(args->proc_fd, LAST_PID_PATH, O_RDWR, 0);
@@ -1507,8 +1518,10 @@ long __export_restore_task(struct task_restore_args *args)
 		for (i = 0; i < args->nr_threads; i++) {
 			char last_pid_buf[16], *s;
 			/* skip self */
-			if (thread_args[i].pid[0] == args->t->pid[0])
+			if (thread_args[i].pid[0] == args->t->pid[0]) {
+				self_thread = i;
 				continue;
+			}
 
 			if (fd >= 0) {
 				/* One level pid ns hierarhy */
@@ -1605,6 +1618,16 @@ long __export_restore_task(struct task_restore_args *args)
 	restore_finish_stage(task_entries_local, CR_STATE_RESTORE_SIGCHLD);
 
 	rst_tcp_socks_all(args);
+
+	fd = args->thread_args[self_thread].pfc_ns_fd;
+	if (fd >= 0) {
+		ret = sys_setns(fd, CLONE_NEWPID);
+		if (ret) {
+			pr_err("Can't setns: ret=%d\n", (int)ret);
+			goto core_restore_end;
+		}
+		sys_close(fd);
+	}
 
 	/* The kernel restricts setting seccomp to uid 0 in the current user
 	 * ns, so we must do this before restore_creds.
